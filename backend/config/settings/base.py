3ea04ea0aeea444be8a149f11bc057767723bcb9
Avemfohn <mertcan.erc@hotmail.com>
2026-05-27 17:12:19 +0000

    Add Whitenoise for static file handling and update configurations
    - Added Whitenoise as a dependency in pyproject.toml and requirements.txt for serving static files.
    - Updated the middleware settings to include Whitenoise for improved static file management.
    - Adjusted STATIC_URL to ensure proper serving of static files in the application.

--- backend/config/settings/base.py
+++ backend/config/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -80,7 +81,7 @@ TIME_ZONE = "Europe/Istanbul"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
